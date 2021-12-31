6943c42242497139fd3cfbdf6f2dd1fb2b24f945
Chickenbiscuitoo <chickenbiscuito.dev@gmail.com>
2021-12-31 21:44:33 +0100

    new functionalities

--- code.js
+++ code.js
@@ -14,7 +14,7 @@ function divide (n1, n2) {
     return n1 / n2;
 }
 
-function operate (operator, n1, n2) {
+function operate (n1, operator, n2) {
     if (operator === '+') {
         return add(n1, n2);
     }
@@ -28,3 +28,40 @@ function operate (operator, n1, n2) {
         return divide(n1, n2);
     }
 }
+
+const calcBtnList = document.getElementsByClassName('calcBtn');
+const btnNumList = document.getElementsByClassName('btnNum');
+const display = document.getElementById('display');
+const displaySolution = document.getElementById('displaySolution');
+
+let displayValue = '';
+
+for (let c = 0; c < calcBtnList.length; c++) {
+    calcBtnList[c].addEventListener('click', () => {
+        displayValue += calcBtnList[c].innerHTML;
+        display.innerHTML = displayValue;
+    })
+}
+
+const btnEquals = document.getElementById('btnEquals');
+btnEquals.addEventListener('click', () => {
+    const operateValue = displayValue.split(" ")
+    console.log(operateValue);
+    console.log(operateValue.length)
+
+
+
+
+    solution = operate(Number(operateValue[0]), operateValue[1], Number(operateValue[2]));
+
+    if (operateValue.length > 3) {
+        for (let i = 3; i <= operateValue.length; i += 2) {
+            solution = operate(solution, operateValue[i], Number(operateValue[i+1]));
+        }
+    }
+
+
+
+    
+    displaySolution.innerHTML = solution;
+})
